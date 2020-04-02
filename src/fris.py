7b596cb3e914a7f624b4cdf36b5d62755abbe713
Beersonthewall <matthew@beersonthewall.com>
2020-04-02 08:46:03 -0500

    Add index.html with form and css.

--- src/fris.py
+++ src/fris.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 
+ALLOWED_EXTENSIONS = ['img', 'jpg']
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/classify")
+@app.route("/classify", methods=['POST'])
 def classify():
+    img = request.files['file_key']
     return "Classify"
