0aa1c07bd4da4fdf628d664212ffb0c03ec10c38
Beersonthewall <matthew@beersonthewall.com>
2020-04-02 07:40:29 -0500

    Add index template and classify route.

--- src/fris.py
+++ src/fris.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/classify")
+def classify():
+    return "Classify"
