b3883ab317b6f38dc6375a26e2e9c073a05184fe
Beersonthewall <matthew@beersonthewall.com>
2020-04-04 17:43:31 -0500

    Error for incorrect file type.

--- src/fris.py
+++ src/fris.py
@@ -4,6 +4,7 @@ from io import BytesIO
 
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 MODEL_PATH = 'model/'
+INDEX_TEMPLATE = "index.html"
 
 app = Flask(__name__)
 learner = load_learner(MODEL_PATH)
@@ -16,20 +17,19 @@ def index():
 @app.route("/classify", methods=['POST'])
 def classify():
     if 'file' not in request.files:
-        return render_template("index.html", error="No file part")
+        return render_template(INDEX_TEMPLATE, error="No file part.")
 
     file = request.files['file']
     if file.filename == '':
-        return render_template("index.html", error="No selected file part")
+        return render_template(INDEX_TEMPLATE, error="No selected file part.")
 
     if file and allowed_file(file.filename):
         bytes = file.read()
         img = open_image(BytesIO(bytes))
         prediction,label,outputs = learner.predict(img)
-        return render_template("index.html", prediction=prediction, confidence=outputs[label])
+        return render_template(INDEX_TEMPLATE, prediction=prediction, confidence=outputs[label])
     else:
-        flash('File type not allowed')
-        return redirect(request.url)
+        return render_template(INDEX_TEMPLATE, error="File type not allowed.")
 
 
 def allowed_file(filename):
